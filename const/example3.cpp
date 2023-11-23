9472a1838e63452d0c1702c50535b4e42bfd5603
AlexGabi17 <alexbuzescu@yahoo.com>
2023-11-23 14:07:11 +0200

    const

---
+++ const/example3.cpp
@@ -0,0 +1,35 @@
+
+
+// C++ program to demonstrate
+// the above concept
+#include <iostream>
+using namespace std;
+ 
+// Driver code
+int main()
+{
+    int x{ 9 };
+    
+    const int* const i = &x;
+   // *i=10;
+    // The above statement will give CTE
+    // Once Ptr(*i) value is
+    // assigned, later it can't
+    // be modified(Error)
+ 
+    char y{ 'A' };
+ 
+    const char* const j = &y;
+ 
+    // *j='B';
+    // The above statement will give CTE
+    // Once Ptr(*j) value is
+    // assigned, later it can't
+    // be modified(Error)
+    
+    x = 24;
+
+    cout << *i << " and " << *j;
+ 
+    return 0;
+}
