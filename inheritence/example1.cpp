a076bc6181b015e1d71a15381659549b728b97ed
AlexGabi17 <alexbuzescu@yahoo.com>
2024-01-08 22:01:48 +0200

    exemplu mostenire

---
+++ inheritence/example1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    public:
+    virtual ~Base(){
+        cout<<"Base1\n";
+        f();
+    }
+    virtual void f(){
+        cout<<"Base:f\n";
+    }
+};
+
+class Derived: public Base{
+    ~Derived(){
+        cout<<"~Derived\n";
+    }
+    void f(){
+        cout<<"Derived:f()\n";
+    }
+};
+
+int main(){
+    Base* bp = new Derived;
+
+    delete bp;
+
+    return 0;
+}
